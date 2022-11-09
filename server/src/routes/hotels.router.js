21a0822e27af69f893a29895b5ca89d010dc31f5
surajkm24 <surajkm336796@gmail.com>
2022-11-09 14:26:01 +0530

    get single hotel data

--- server/src/routes/hotels.router.js
+++ server/src/routes/hotels.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getHotels } = require('../controllers/hotels.controller');
+const { getHotels, getSingleHotel } = require('../controllers/hotels.controller');
 
 const app = express.Router();
 
 app.get('/', getHotels);
+
+app.get('/:id', getSingleHotel)
 
 module.exports = app;
