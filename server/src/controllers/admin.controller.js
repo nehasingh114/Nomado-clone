203031a1bb2728104d325bd1971ba597aae1275d
surajkm24 <surajkm336796@gmail.com>
2022-11-12 09:48:17 +0530

    get users and delete user

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/users.model.js');
+const Admin = require('../models/admin.model.js');
 const Token = require('../models/tokens.model.js');
+const Hotel = require('../models/hotels.model.js')
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -8,29 +10,61 @@ const tokenSecretKey = process.env.ADMIN_TOKEN;
 const adminLogin = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const admin = await User.findOne({ email, password,role:"admin" });
-        console.log(admin)
-        console.log(1)
+        const admin = await Admin.findOne({ email, password });
         const token = jwt.sign({ email, role: admin.role }, tokenSecretKey);
-        console.log(2)
-        admin.delete('password')
-        console.log(3,admin);
         return res.send({ message: "Login success.", admin, token })
     }
     catch (e) {
-        return res.status(404).send({ message: 'Unauthenticated',e })
+        return res.status(404).send({ message: 'Unauthenticated', e })
     }
 }
 
-const getUsers = async(req,res) => {
-    const {token} = req.headers;
-    try{
-        const admin = jwt.verify(token,tokenSecretKey);
-        const users = await User.find({},{password:0});
-        return res.send({message:"Success",data:users})
+const getUsers = async (req, res) => {
+    const { token } = req.headers;
+    try {
+        const admin = jwt.verify(token, tokenSecretKey);
+        const users = await User.find({}, { password: 0 });
+        return res.send({ message: "Success", data: users })
     }
-    catch(e){
-        return res.status(401).send({message:"Error",e})
+    catch (e) {
+        return res.status(401).send({ message: "Error", e })
     }
 }
-module.exports = { adminLogin,getUsers };
+
+const deleteUser = async (req, res) => {
+    const { token } = req.headers;
+    const { id } = req.params;
+    try {
+        const admin = jwt.verify(token, tokenSecretKey);
+        const user = await User.findByIdAndDelete(id);
+        return res.send({ message: "Success", data: user })
+    }
+    catch (e) {
+        return res.status(401).send({ message: "Error", e })
+    }
+}
+
+const getHotels = async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        return res.send({ message: "Success", data: hotels })
+    }
+    catch (e) {
+        return res.status(401).send({ message: "Error", e })
+    }
+}
+
+const deleteHotel = async (req, res) => {
+    const { token } = req.headers;
+    const { id } = req.params;
+    try {
+        const admin = jwt.verify(token, tokenSecretKey);
+        const hotel = await Hotel.findByIdAndDelete(id);
+        return res.send({ message: "Success", data: hotel })
+    }
+    catch (e) {
+        return res.status(401).send({ message: "Error", e })
+    }
+}
+
+module.exports = { adminLogin, getUsers, deleteUser, getHotels, deleteHotel };
