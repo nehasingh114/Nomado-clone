63ceaf181cfab43463cba6d7e32dd4cafa428ed2
surajkm24 <surajkm336796@gmail.com>
2022-11-12 10:46:20 +0530

    admin data

--- server/src/models/hotels.model.js
+++ server/src/models/hotels.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const hotelSchema = new mongoose.Schema({
 
-})
+}, { strict: false })
 const Hotel = mongoose.model('hotel', hotelSchema)
 
 module.exports = Hotel;
